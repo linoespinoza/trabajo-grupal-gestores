f89c3012f48318588a9dd43496d7f969f3d7a12e
Harry Bravo <hbravocoronel@gmail.com>
2014-04-05 09:22:33 -0500

    Se corrigio las tildes que salía con basurita

--- src/gestores/constante/GeneralConstante.java
+++ src/gestores/constante/GeneralConstante.java
@@ -10,17 +10,17 @@ public class GeneralConstante {
 	private GeneralConstante() {
 	}
 
-	public static final String ERROR_GENERAL = "Hubo un error en el sistema. Cont√°ctese con el administrador.";
+	public static final String ERROR_GENERAL = "Hubo un error en el sistema. Cont·ctese con el administrador.";
 	public static final String ERROR_CONEXION_BASE_DATOS = "No se pudo conectar con la base de datos.";
 
-	public static final String MSJ_VALID_DNI = "El dni debe ser num√©rico de "
+	public static final String MSJ_VALID_DNI = "El dni debe ser numÈrico de "
 			+ TipoDocumento.DNI.getLongitud() + " digitos";
-	public static final String MSJ_VALID_RUC = "El ruc debe ser num√©rico de "
+	public static final String MSJ_VALID_RUC = "El ruc debe ser numÈrico de "
 			+ TipoDocumento.RUC.getLongitud() + " digitos";
-	public static final String MSJ_VALID_PASAPORTE = "El pasaporte debe ser alfanum√©rico de "
+	public static final String MSJ_VALID_PASAPORTE = "El pasaporte debe ser alfanumÈrico de "
 			+ TipoDocumento.PASAPORTE.getLongitud() + " digitos";
-	public static final String MSJ_VALID_CARNET_EXTRANJERIA = "El carnet de extranjer√≠a debe ser alfanum√©rico de "
+	public static final String MSJ_VALID_CARNET_EXTRANJERIA = "El carnet de extranjerÌa debe ser alfanumÈrico de "
 			+ TipoDocumento.CARNET_EXTRANJERIA.getLongitud() + " digitos";
-	public static final String MSJ_VALID_PARTIDA_NACIMIENTO = "La partida de nacimiento debe ser alfanum√©rico de "
+	public static final String MSJ_VALID_PARTIDA_NACIMIENTO = "La partida de nacimiento debe ser alfanumÈrico de "
 			+ TipoDocumento.PARTIDA_NACIMIENTO.getLongitud() + " digitos";
 }
